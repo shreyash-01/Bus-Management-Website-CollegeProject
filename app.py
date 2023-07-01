9b5d61f308c0ab5fc9c5593a9cf3aab5fbf2d611
shreyash-01 <singhshreyash0075@gmail.com>
2023-07-01 21:09:03 +0530

    added requirements.txt and implemented routing

--- app.py
+++ app.py
@@ -8,6 +8,15 @@ def home_page():  # put application's code here
     return render_template('home.html')
 
 
+@app.route('/account')
+def user_account():
+    return "this is account page"
+
+
+@app.route('/dashboard')
+def dashboard_page():
+    return "this is dashboard"
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
